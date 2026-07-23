68ca74738a216edaac0daf5bc9a80cf2600203ed
Jr-R-007 <rupesh.2405429@saketcollege.edu.in>
2026-07-23 17:08:20 +0530

    a

---
+++ FSD/TYIT_Node.js/1_d.js
@@ -0,0 +1,20 @@
+let num1=15;
+let num2=25;
+
+function add(a,b){
+    return a+b;
+}
+
+let result=add(num1,num2);
+
+console.log("First Number: " ,num1);
+console.log("Second Number: " ,num2);
+console.log("Sum : ",result);
+
+const readline interface 
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});     
+
+function 
